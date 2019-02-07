f2e0890639a5c93763ef68226bc78b4c66010b22
Andy <wang.andy32@gmail.com>
2019-02-06 20:20:50 -0500

    form done

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-window.alert("Yo");
+  let form = document.getElementById("data-form");
+
+
+  let handleClick = function(event){
+    event.preventDefault();
+
+    let name = event.currentTarget.name.value;
+    let age = event.currentTarget.age.value;
+    let color = event.currentTarget.color.value;
+
+    console.log("Hi my name is "+name+" and I am "+ age+" my favorite color is " + color);
+    console.log(event.target.name.innerText);
+  }
+
+  form.addEventListener("submit", handleClick);
